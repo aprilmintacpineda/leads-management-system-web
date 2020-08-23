de2fa4eb0b119e6d5a5e276d1b2589baf958c1e5
April Mintac Pineda <aprilmintacpineda@gmail.com>
2020-08-23 22:44:30 +0800

    updateUserGroups mutation and lambda function;

---
+++ amplify/backend/function/updateUserGroups/src/index.js
@@ -0,0 +1,66 @@
+/* Amplify Params - DO NOT EDIT
+  API_LEADSMANAGEMENTSYS_GRAPHQLAPIENDPOINTOUTPUT
+  API_LEADSMANAGEMENTSYS_GRAPHQLAPIIDOUTPUT
+  API_LEADSMANAGEMENTSYS_GRAPHQLAPIKEYOUTPUT
+  API_LEADSMANAGEMENTSYS_USERTABLE_ARN
+  API_LEADSMANAGEMENTSYS_USERTABLE_NAME
+  AUTH_LEADSMANAGEMENTSYS71FD0E2271FD0E22_USERPOOLID
+  ENV
+  REGION
+  STORAGE_S3453D1BD4_BUCKETNAME
+Amplify Params - DO NOT EDIT */
+
+const aws = require('aws-sdk');
+
+const CognitoISP = new aws.CognitoIdentityServiceProvider();
+
+exports.handler = async ({
+  arguments: { groups: newGroups, id },
+  identity: { groups: requestorGroups }
+}) => {
+  console.log('groups', newGroups);
+  console.log('id', id);
+
+  if (!requestorGroups.includes('Admin')) throw new Error('Unauthorized');
+
+  const poolId = process.env.AUTH_LEADSMANAGEMENTSYS71FD0E2271FD0E22_USERPOOLID;
+
+  let { Groups: oldGroups } = await CognitoISP.adminListGroupsForUser({
+    UserPoolId: poolId,
+    Username: id
+  }).promise();
+
+  oldGroups = oldGroups.reduce(
+    ({ GroupName }, accumulator) => accumulator.concat(GroupName),
+    []
+  );
+
+  console.log('oldGroups', oldGroups);
+
+  await Promise.all(
+    [].concat(
+      // adding user to new groups
+      newGroups.map(group => {
+        if (oldGroups.includes(group)) return null;
+
+        return CognitoISP.adminAddUserToGroup({
+          UserPoolId: poolId,
+          Username: id,
+          GroupName: group
+        }).promise();
+      }),
+      // removing user from groups
+      oldGroups.map(group => {
+        if (newGroups.includes(group)) return null;
+
+        return CognitoISP.adminRemoveUserFromGroup({
+          UserPoolId: poolId,
+          Username: id,
+          GroupName: group
+        }).promise();
+      })
+    )
+  );
+
+  return { status: 'success' };
+};
